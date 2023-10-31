4f6795b2c39529c152e5261334a8e1a2b4701d02
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-10-31 21:20:23 +0200

    my first commit

---
+++ PR-6.3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+
+		cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+	cout << endl;
+}
+
+int Sum(int* a, const int size)
+{
+	int S = 0;
+	for (int i = 0; i < size; i++)
+
+		S += a[i];
+
+	return S;
+}
+
+int main()
+{
+	const int n = 8;
+	int a[n];
+	int Low = -4;
+	int High = 4;
+
+	Create(a, n, Low, High);
+	Print(a, n);
+
+	cout << "S = " << Sum(a, n) << endl;
+
+	return 0;
+}
+
+
